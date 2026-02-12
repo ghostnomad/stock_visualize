01f0e6a6a0c2724bc2d34035510046ed813adfbb
ghostnomad <ghostnomad@gmail.com>
2026-02-12 09:36:16 -0500

    updates to multi stock pull

--- stock_charts.py
+++ stock_charts.py
@@ -15,18 +15,27 @@ ta_client = StockHistoricalDataClient(api_key, secret_key)
 
 # add the ability to set the time frame to build the chart
 
-request_params = StockBarsRequest(
+request_params1 = StockBarsRequest(
     symbol_or_symbols=symbol,
     timeframe=TimeFrame.Day,
     start=datetime(2026, 1, 11), # Start date
     end=datetime(2026, 2, 12)    # End date
 )
 
-bars = ta_client.get_stock_bars(request_params)
+request_params2 = StockBarsRequest(
+    symbol_or_symbols="WMT",
+    timeframe=TimeFrame.Day,
+    start=datetime(2026, 1, 11),
+    end=datetime(2026, 2, 12)
+)
 
-df = bars.df
+bars1 = ta_client.get_stock_bars(request_params1)
+bars2 = ta_client.get_stock_bars(request_params2)
+
+df1 = bars1.df
+df2 = bars2.df
 #this is just for testing purposes
-print(df)
+print(df2)
 
 #latest_close = df['close'].iloc[-1]
 #print(f"The EOD Close Price for AAPL was: ${latest_close}")
